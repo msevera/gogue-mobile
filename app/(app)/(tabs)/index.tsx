fe04cd1a39d8cb25074674765ecc97d115b89f58
Mike Severa <michael.svr@gmail.com>
2025-06-28 00:08:18 +0200

    Update HomeScreen component to redirect to lectures instead of search
    - Modified the HomeScreen component to redirect users to the lectures page by default.
    - Commented out the previous redirect to the search page for clarity and future reference.

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,8 +1,8 @@
 import { Redirect } from 'expo-router';
 
 export default function HomeScreen() {
-  return <Redirect href="/search" />
-  // return <Redirect href="/lectures" />
+  // return <Redirect href="/search" />
+  return <Redirect href="/lectures" />
   // return <Redirect href="/lectures/685aa30c1dd5623fccff2150" />
   // return <Redirect href="/685aa30c1dd5623fccff2150" />
 }
